4f7c0f0d2ec3a7a3c44b047a636a12ba5c859009
jimbobding <jimmygrahamdavid@gmail.com>
2022-06-22 17:05:11 +0000

    fixed about section and added map

--- js/file.js
+++ js/file.js
@@ -1,1 +1,10 @@
 AOS.init();
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
