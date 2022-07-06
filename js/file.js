baf966485e0347f382b8170c4ced8b7034488605
jimbobding <jimmygrahamdavid@gmail.com>
2022-07-06 12:57:12 +0000

    changed hero picture and aos on about pictures

--- js/file.js
+++ js/file.js
@@ -1,10 +1,2 @@
 AOS.init();
 
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
